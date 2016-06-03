940e1f8f39a1d56ae9bc23dc280e31e242fe0322
Grace <grace.h.gamble@gmail.com>
2016-06-03 00:29:49 +0000

    Loop Practice #2

---
+++ main.js
@@ -0,0 +1,32 @@
+//Part 1
+/*var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+//returns rat cat butterfly marmot
+for (var i = 0; i < animals.length-1; i++) {
+    console.log(animals[i]);
+}*/
+
+//Part 2 
+/*var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+//returns rat butterfly ocelot
+for (var i = 0; i < animals.length; i+=2) {
+    console.log(animals[i]);
+}*/
+
+//Part 3
+/*var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+//returns ocelot marmot butterfly cat rat
+for (var i = animals.length-1; i >= 0; i--) {
+    console.log(animals[i]);
+}*/
+
+//Part 4
+/*var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+//returns rat cat cat butterfly butterfly marmot marmot ocelot
+for (var i = 0; i < animals.length; i++) {
+    for (var j = 0; j <= 0; j++) {
+        if (!(i == 0 || i == animals.length - 1)) {
+        console.log(animals[i]);
+    }
+   console.log(animals[i]);
+ }
+}*/
